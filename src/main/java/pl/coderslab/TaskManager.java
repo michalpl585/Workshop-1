3e74f6ab090bca31c984149e5c7c32d5b7736cfa
michalpl585 <michalpl585@interia.pl>
2024-02-21 21:33:08 +0100

    michalpl585

--- src/main/java/pl/coderslab/TaskManager.java
+++ src/main/java/pl/coderslab/TaskManager.java
@@ -1,2 +1,19 @@
-package pl.coderslab;public class TaskManager {
+package pl.coderslab;
+
+import java.util.Arrays;
+
+public class TaskManager {
+    public static void main(String[] args) {
+        taskShowOptions();
+    }
+
+    static void taskShowOptions() {
+        String[] taskOptions = {"add", "remove", "list", "exit"};
+
+        System.out.println(ConsoleColors.BLUE + "Please select an option:" + ConsoleColors.RESET);
+        for(String s : taskOptions) {
+            System.out.println(s);
+        }
+
+    }
 }
